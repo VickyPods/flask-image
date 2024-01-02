a7c92988b166a8fcb31c5340207f645793174cee
vicky <vicky@gmail.com>
2024-01-02 12:50:38 +0530

    add

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "Logging in to Amazon ECR..."
 aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 047185742847.dkr.ecr.us-west-1.amazonaws.com
 echo "Logged in to Amazon ECR successfully."
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+echo "Running container..."
 docker run -d -p 5000:5000 047185742847.dkr.ecr.us-west-1.amazonaws.com/flask-image:latest
